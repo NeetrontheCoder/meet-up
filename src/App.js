2a0ca7e4d26fcf0ffec42517f17eadc2d88a0ede
Neetu <upneetneetu@gmail.com>
2022-08-07 23:20:41 +0200

    created routes and navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavouritesPage from './pages/Favourites';
+import MainNavigation from './components/layouts/MainNavigation';
+
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetups" element={<NewMeetupPage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
